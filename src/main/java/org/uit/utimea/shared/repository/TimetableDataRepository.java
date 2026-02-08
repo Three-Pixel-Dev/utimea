f95fe5656295521f4694707d3823da889fbf185d
thanthtooaung-coding <thanthtoo1285@gmail.com>
2026-02-08 15:13:51 +0700

    :zap: added domain for timetable process

---
+++ src/main/java/org/uit/utimea/shared/repository/TimetableDataRepository.java
@@ -0,0 +1,8 @@
+package org.uit.utimea.shared.repository;
+
+import org.springframework.stereotype.Repository;
+import org.uit.utimea.shared.entity.TimetableData;
+
+@Repository
+public interface TimetableDataRepository extends BaseRepository<TimetableData> {
+}
---
+++ src/main/java/org/uit/utimea/shared/repository/TimetableInfoRepository.java
@@ -0,0 +1,8 @@
+package org.uit.utimea.shared.repository;
+
+import org.springframework.stereotype.Repository;
+import org.uit.utimea.shared.entity.TimetableInfo;
+
+@Repository
+public interface TimetableInfoRepository extends BaseRepository<TimetableInfo> {
+}
